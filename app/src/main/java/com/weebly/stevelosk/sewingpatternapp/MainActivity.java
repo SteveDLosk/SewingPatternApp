c88d8b3d198530a1e901118660dee83c158a3174
Steve Losk <Stevedlosk@gmail.com>
2018-01-25 21:06:23 -0800

    MainActivity top right button launches AddPatternActivity

--- app/src/main/java/com/weebly/stevelosk/sewingpatternapp/MainActivity.java
+++ app/src/main/java/com/weebly/stevelosk/sewingpatternapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.weebly.stevelosk.sewingpatternapp;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton topRightButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        topRightButton = (ImageButton) findViewById(R.id.imgButtonTopRight);
+        topRightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addIntent = new Intent(getApplicationContext(), AddPatternActivity.class);
+                startActivity(addIntent);
+            }
+        });
     }
 }
