618cd4ad4110594581d02ad11b349f58a6782447
Steve Losk <Stevedlosk@gmail.com>
2018-01-25 20:48:12 -0800

    Created AddPatternActivity.  Also working with String resources.

---
+++ app/src/main/java/com/weebly/stevelosk/sewingpatternapp/AddPatternActivity.java
@@ -0,0 +1,13 @@
+package com.weebly.stevelosk.sewingpatternapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPatternActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pattern);
+    }
+}
