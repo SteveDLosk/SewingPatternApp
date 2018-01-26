3b6be95712b66bbf890e376f80c96762c758b44f
Steve Losk <Stevedlosk@gmail.com>
2018-01-26 11:50:26 -0800

    Added camera picture take on first image.  Crashes on image accepted.. still working on it.

--- app/src/main/java/com/weebly/stevelosk/sewingpatternapp/AddPatternActivity.java
+++ app/src/main/java/com/weebly/stevelosk/sewingpatternapp/AddPatternActivity.java
@@ -1,13 +1,55 @@
 package com.weebly.stevelosk.sewingpatternapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AddPatternActivity extends AppCompatActivity {
+
+    static final int REQUEST_FRONT_IMAGE_CAPTURE = 1;
+
+    private ImageView frontPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pattern);
+
+        ImageView frontPic = (ImageView) findViewById(R.id.frontImage);
+        frontPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent(REQUEST_FRONT_IMAGE_CAPTURE);
+            }
+        });
+
+
+    }
+
+
+
+    private void dispatchTakePictureIntent(int id) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, id);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_FRONT_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            System.out.println("Got the pciture");
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            frontPic.setImageBitmap(imageBitmap);
+        }
+        else if (requestCode == REQUEST_FRONT_IMAGE_CAPTURE && resultCode == RESULT_CANCELED) {
+            System.out.println("User canceled the activity");
+        }
     }
 }
