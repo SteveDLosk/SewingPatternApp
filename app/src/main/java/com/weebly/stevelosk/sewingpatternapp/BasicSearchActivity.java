7b3738e9f9f9fa2ff4d7394d4c6bf71b73de3309
Steve Losk <Stevedlosk@gmail.com>
2018-02-03 20:43:18 -0800

    Search is run using AsyncTask.  That works very well, and updates the UI correctly. I just don't yet know how I want to call the AsyncTask.  Currently called on touch of placeholder image.  Not sure if I want to keep it that way.  Still, the AsyncTask itself works perfectly!

--- app/src/main/java/com/weebly/stevelosk/sewingpatternapp/BasicSearchActivity.java
+++ app/src/main/java/com/weebly/stevelosk/sewingpatternapp/BasicSearchActivity.java
@@ -1,13 +1,18 @@
 package com.weebly.stevelosk.sewingpatternapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -16,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -36,6 +42,10 @@ public class BasicSearchActivity extends AppCompatActivity {
     private Timer textChangedTimer;
     private int TIMER_DELAY = 50;
 
+    private PatternAdapter pa = null;
+
+    private final String TAG = "SDL BasicSearchActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +57,7 @@ public class BasicSearchActivity extends AppCompatActivity {
 
         // ListView
         resultsListView = (ListView) findViewById(R.id.basicSearch_ListView);
-        PatternAdapter pa = new PatternAdapter(this, patterns);
+        pa = new PatternAdapter(this, patterns);
         resultsListView.setAdapter(pa);
 
         resultsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -62,6 +72,18 @@ public class BasicSearchActivity extends AppCompatActivity {
             }
         });
 
+        placeHolderImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AsyncTask searchTask = new AsyncSearchTask();
+                Object[] params = {getApplicationContext(),
+                        searchEditText.getText().toString(), patterns};
+                searchTask.execute(params);
+
+            }
+        });
+
+        /*
         // TODO: use Handler instead?
         searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -92,6 +114,9 @@ public class BasicSearchActivity extends AppCompatActivity {
     private void search() {
         try {
             // remove prior search results
+            Object o0 = getApplicationContext();
+            Object o1 = searchEditText.getText().toString();
+            Object o2 = patterns;
             patterns.clear();
             PatternDBAdapter db = new PatternDBAdapter(getApplicationContext());
             db.open();
@@ -110,6 +135,67 @@ public class BasicSearchActivity extends AppCompatActivity {
 
         catch(Exception e) {
             resultsTextView.setText(e.getMessage());
+        }
+    }
+    */
+    }
+
+     class AsyncSearchTask extends AsyncTask<Object, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Object... objects) {
+
+            // create database connection
+            Context ctx = (Context) objects[0];
+            PatternDBAdapter db = new PatternDBAdapter(ctx);
+
+            // get query content and result set target from packaged objects
+            String searchStr = (String) objects[1];
+            ArrayList<Pattern> patterns = (ArrayList<Pattern>) objects[2];
+
+            try {
+
+                // clear the old result list
+                patterns.clear();
+
+                // open the database
+                db.open();
+
+                // query
+                String[] predicate = new String[1];
+                predicate[0] = searchStr;
+                Cursor cursor = db.getPatternByID(predicate);
+
+                // populate the results from the query result set
+                while (cursor.moveToNext()) {
+                    Pattern p = PatternDBAdapter.getPatternFromCursor(cursor);
+                    patterns.add(p);
+                }
+
+                return 0;
+            } catch (SQLException e) {
+                Toast.makeText(ctx, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                return 1;
+            } finally {
+                db.close();
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+
+            if (result == 0) {
+                pa.notifyDataSetChanged();
+                if (patterns.size() == 0) {
+                    Toast.makeText(getApplicationContext(), R.string.noPatternsFound,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(getApplicationContext(), R.string.database_error,
+                        Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
